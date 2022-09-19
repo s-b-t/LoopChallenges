d36595b6992bc33b97773ba682f5a0261ad3ef41
Steve-Tobias <sblaket@gmail.com>
2022-09-19 16:00:31 -0600

    Loop_Challenges

---
+++ loopchallenges.js
@@ -0,0 +1,51 @@
+// 1. Print odds 1-20: Using a loop write code that will console.log 
+// all of the odd values from 1 up to 20.
+
+// 2. Decreasing Multiples of 3: Write a loop that will console.log 
+// all values evenly divisible by 3 from 100 down to 0
+
+// 3. Print the sequence using a loop that will console.log 
+// the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5
+
+// 4. Sigma: Write code that will add all of the values from 1-100 
+// onto some variable sum and at the end console.log 
+// the result 1 + 2 + 3 + ... + 98 + 99 + 100. 
+// We should get back 5050 at the end.
+
+// 5. Factorial: Write code that will multiply all of the values from 1-12 
+// onto some variable product and at the end console.log 
+// the result 1 * 2 * 3 * ... * 10 * 11 * 12. 
+// We should get back 479001600 at the end.
+
+ for(var i = 1; i <= 20; i++){
+    if(i % 2 == 1) {
+    console.log(i)
+    }
+}
+
+for(var i = 100; i >= 0; i--){
+    if(i % 3 == 0) {
+    console.log(i)
+    }
+}
+
+for(var i = 4; i >= -3.5; i -= 1.5){
+    console.log(i)
+}
+
+var sum = 0;
+for(var i = 1; i <= 100; i++){
+    sum = i + sum;
+}
+console.log(sum);
+
+var product = 1;
+for(var i = 1; i <= 12; i++){
+    product = i * product;
+}
+console.log(product);
+
+
+
+
+
